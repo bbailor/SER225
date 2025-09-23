f0a2993a64de39a35b6006d6b14af6763ac310fe
bbailor <bensamb06@gmail.com>
2025-09-23 10:56:56 -0400

    Added a functioning controls option to the menu screen that displays the basic controls of the game

--- src/Screens/MenuScreen.java
+++ src/Screens/MenuScreen.java
@@ -6,7 +6,6 @@ import Game.ScreenCoordinator;
 import Level.Map;
 import Maps.TitleScreenMap;
 import SpriteFont.SpriteFont;
-
 import java.awt.*;
 
 // This is the class for the main menu screen
@@ -16,9 +15,11 @@ public class MenuScreen extends Screen {
     protected int menuItemSelected = -1;
     protected SpriteFont playGame;
     protected SpriteFont credits;
+    protected SpriteFont controls;
     protected Map background;
     protected int keyPressTimer;
     protected int pointerLocationX, pointerLocationY;
+    //protected int cursorLocationX, cursorLocationY;
     protected KeyLocker keyLocker = new KeyLocker();
 
     public MenuScreen(ScreenCoordinator screenCoordinator) {
@@ -33,6 +34,9 @@ public class MenuScreen extends Screen {
         credits = new SpriteFont("CREDITS", 200, 223, "Arial", 30, new Color(49, 207, 240));
         credits.setOutlineColor(Color.black);
         credits.setOutlineThickness(3);
+        controls = new SpriteFont("CONTROLS", 200, 323, "Arial", 30, new Color(49, 207, 240));
+        controls.setOutlineColor(Color.black);
+        controls.setOutlineThickness(3);
         background = new TitleScreenMap();
         background.setAdjustCamera(false);
         keyPressTimer = 0;
@@ -58,23 +62,32 @@ public class MenuScreen extends Screen {
         }
 
         // if down is pressed on last menu item or up is pressed on first menu item, "loop" the selection back around to the beginning/end
-        if (currentMenuItemHovered > 1) {
+        if (currentMenuItemHovered > 2) {
             currentMenuItemHovered = 0;
         } else if (currentMenuItemHovered < 0) {
-            currentMenuItemHovered = 1;
+            currentMenuItemHovered = 2;
         }
 
         // sets location for blue square in front of text (pointerLocation) and also sets color of spritefont text based on which menu item is being hovered
         if (currentMenuItemHovered == 0) {
             playGame.setColor(new Color(255, 215, 0));
             credits.setColor(new Color(49, 207, 240));
+            controls.setColor(new Color(49, 207, 240));
             pointerLocationX = 170;
             pointerLocationY = 130;
         } else if (currentMenuItemHovered == 1) {
             playGame.setColor(new Color(49, 207, 240));
             credits.setColor(new Color(255, 215, 0));
+            controls.setColor(new Color(49, 207, 240));
             pointerLocationX = 170;
             pointerLocationY = 230;
+        }
+        else if (currentMenuItemHovered == 2) {
+            playGame.setColor(new Color(49, 207, 240));
+            credits.setColor(new Color(49, 207, 240));
+            controls.setColor(new Color(255, 215, 0));
+            pointerLocationX = 170;
+            pointerLocationY = 330;
         }
 
         // if space is pressed on menu item, change to appropriate screen based on which menu item was chosen
@@ -88,6 +101,10 @@ public class MenuScreen extends Screen {
             } else if (menuItemSelected == 1) {
                 screenCoordinator.setGameState(GameState.CREDITS);
             }
+            else if (menuItemSelected == 2)
+            {
+                screenCoordinator.setGameState(GameState.CONTROLS);
+            }
         }
     }
 
@@ -95,6 +112,7 @@ public class MenuScreen extends Screen {
         background.draw(graphicsHandler);
         playGame.draw(graphicsHandler);
         credits.draw(graphicsHandler);
+        controls.draw(graphicsHandler);
         graphicsHandler.drawFilledRectangleWithBorder(pointerLocationX, pointerLocationY, 20, 20, new Color(49, 207, 240), Color.black, 2);
     }
 }
