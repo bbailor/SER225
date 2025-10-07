853fa3a169fc9996b7f7605b17ace58ce7a804e8
TheTitanOfTime <aurelien.buisine@icloud.com>
2025-10-07 19:29:50 -0400

    Set the player gnome variables to privates statics in the Gnome class. Shifted the hitbox a bit

--- src/Players/Gnome.java
+++ src/Players/Gnome.java
@@ -9,9 +9,15 @@ import Level.Player;
 
 import java.util.HashMap;
 
+
 // This is the class for the Gnome player character
 // basically just sets some values for physics and then defines animations
 public class Gnome extends Player {
+
+    private static int boundY = 21;
+    private static int boundX = 4;
+    private static int boundWidth = 23;
+    private static int boundHeight = 37;
 
 
     public Gnome(float x, float y) {
@@ -33,7 +39,7 @@ public class Gnome extends Player {
             put("STAND_LEFT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(0, 0))
                             .withScale(2)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build()
             });
 
@@ -41,26 +47,26 @@ public class Gnome extends Player {
                     new FrameBuilder(spriteSheet.getSprite(0, 0))
                             .withScale(2)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build()
             });
 
             put("WALK_LEFT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(0, 0), 14)
                             .withScale(2)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 1), 14)
                             .withScale(2)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 2), 14)
                             .withScale(2)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 3), 14)
                             .withScale(2)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build()
             });
 
@@ -68,22 +74,22 @@ public class Gnome extends Player {
                     new FrameBuilder(spriteSheet.getSprite(0, 0), 14)
                             .withScale(2)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 1), 14)
                             .withScale(2)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 2), 14)
                             .withScale(2)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 3), 14)
                             .withScale(2)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
-                            .withBounds(4, 11, 24, 42)
+                            .withBounds(boundX, boundY, boundWidth, boundHeight)
                             .build()
             });
         }};
